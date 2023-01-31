9657148c6a368a75e2112a8a65f53a744bcd3d79
YOLBULAN\bim03 <mgul@yolbulan.com.tr>
2023-01-31 17:40:25 +0300

    31012023

--- WebSocketServer/Program.cs
+++ WebSocketServer/Program.cs
@@ -23,7 +23,7 @@ namespace WebSocketServerApp
                 rk.DeleteValue("VWDWebSocket", false);
             }
 
-            Console.Title = "VWD WebSocket Server";
+            Console.Title = "VWD WebSocket Server 0.2";
 
             string url = $@"ws://{ReadSettings.FromConfig("ipadress")}:{ReadSettings.FromConfig("port")}";
 
@@ -34,11 +34,13 @@ namespace WebSocketServerApp
                 //wssv.AddWebSocketService<Echo>("/Echo");
                 wssv.AddWebSocketService<Entegrasyon>("/Entegrasyon");
                 wssv.AddWebSocketService<HR>("/HR");
+                wssv.AddWebSocketService<Sevkiyat>("/Sevkiyat");
 
                 wssv.Start();
                 //Console.WriteLine($"WS server started on {url}/Echo");
                 Console.WriteLine($"WS server started on {url}/Entegrasyon");
                 Console.WriteLine($"WS server started on {url}/HR");
+                Console.WriteLine($"WS server started on {url}/Sevkiyat");
 
                 Console.ReadKey();
 
@@ -71,7 +73,14 @@ namespace WebSocketServerApp
             Sessions.Broadcast(e.Data);
         }
     }
-
+    public class Sevkiyat : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Sevkiyat client: " + e.Data);
+            Sessions.Broadcast(e.Data);
+        }
+    }
     public class HR : WebSocketBehavior
     {
         protected override void OnMessage(MessageEventArgs e)
